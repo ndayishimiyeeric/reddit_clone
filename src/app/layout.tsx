3d9e8ec57a963354b1f25b360b7c38ce18008425
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-27 07:15:00 +0200

    render navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
 import React from "react";
+import { Inter } from "next/font/google"
+import {cn} from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+import {Toaster} from "@/components/ui/Toaster";
 
 export const metadata = {
   title: 'Reddit Clone',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
+
+const inter = Inter({subsets: ['latin']})
 
 export default function RootLayout({
   children,
@@ -12,8 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en' className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        <Navbar />
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+          {children}
+        </div>
+        <Toaster />
+      </body>
     </html>
   )
 }
