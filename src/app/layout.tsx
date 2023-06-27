d70d9bd136090a06c578d4bb220b71feac139967
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-27 03:03:22 +0200

    Add starter code and utils

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import '@/styles/globals.css'
+import React from "react";
+
+export const metadata = {
+  title: 'Reddit Clone',
+  description: 'A Reddit clone built with Next.js and TypeScript.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  )
+}
