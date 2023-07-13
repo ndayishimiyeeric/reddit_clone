e647a623353ed5a9fb530b811dc1fe18c3a9773e
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-13 02:17:09 +0200

    add the settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {getAuthSession} from "@/lib/auth";
+import {redirect} from "next/navigation";
+import UserNameForm from "@/components/UserNameForm";
+
+export const metadata = {
+    title: "Settings",
+    description: "Manage your account settings",
+}
+
+const Page = async () => {
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+        redirect("/sign-in")
+    }
+    return (
+        <div className="max-w-4xl mx-auto py-12">
+            <div className="grid items-start gap-8">
+                <h1 className="font-bold text-3xl md:text-4xl">Settings</h1>
+            </div>
+
+            <div className="grid gap-10">
+                <UserNameForm session={session}/>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
